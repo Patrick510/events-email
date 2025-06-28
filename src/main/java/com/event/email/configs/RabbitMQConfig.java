70c7ed0d567f9ba33bdc1d7874550185ed792cc6
Patrick510 <patrickdedinho321xz@gmail.com>
2025-06-28 12:37:15 -0400

    Add RabbitMQ topic exchange and binding configuration

--- src/main/java/com/event/email/configs/RabbitMQConfig.java
+++ src/main/java/com/event/email/configs/RabbitMQConfig.java
@@ -1,10 +1,13 @@
 package com.event.email.configs;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 
 @Configuration
 public class RabbitMQConfig {
@@ -12,9 +15,22 @@ public class RabbitMQConfig {
     @Value("${spring.rabbitmq.queue}")
     private String queue;
 
+    public static final String EXCHANGE = "email-exchange";
+    public static final String ROUTING_KEY = "email-routing-key";
+
     @Bean
     public Queue queue() {
         return new Queue(queue, true);
+    }
+
+    @Bean
+    public TopicExchange topicExchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange topicExchange) {
+        return BindingBuilder.bind(queue).to(topicExchange).with(ROUTING_KEY);
     }
 
     @Bean
